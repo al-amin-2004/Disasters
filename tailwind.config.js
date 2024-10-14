ac1d0feadfcdcd7815a11a23f9a5442b4c8ce4b0
Al amin <alaminmridha2004@gmail.com>
2024-10-14 22:38:43 +0600

    3 Color Theme Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F26922",
+        secondary: "#09090B",
+        accent: "#71717A",
+      }
+    },
   },
   plugins: [],
 }
