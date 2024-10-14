c3eebfde6656e0f64c811de6fe0ad94c13e32f2f
Al amin <alaminmridha2004@gmail.com>
2024-10-14 23:39:42 +0600

    7 Container Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Button } from './component/common/button'
+import { Container } from './component/common/ontainer'
 import { SectionTitle } from './component/common/section-title'
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
   return (
     <>
       <main className='font-onest'>
-        <Button>Cypher AI</Button>
-        <SectionTitle>Dashboard</SectionTitle>
+        <Container>
+          <Button>Cypher AI</Button>
+          <SectionTitle>Dashboard</SectionTitle>
+        </Container>
       </main>
     </>
   )
